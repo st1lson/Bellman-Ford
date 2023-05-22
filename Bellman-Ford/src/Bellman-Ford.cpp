87cd1036785c56fe0e01abe5dcb68a44c8e43954
Maksym <rendon4ik@gmail.com>
2023-05-22 21:46:48 +0300

    Code clean up

--- Bellman-Ford/src/Bellman-Ford.cpp
+++ Bellman-Ford/src/Bellman-Ford.cpp
@@ -24,30 +24,4 @@ int main()
 
 	auto tester = TestGenerator(adjacencyMatrix);
 	tester.test();
-
-	//vector<vector<int>> adjacencyMatrix {
-	//	{ 0, 3, 8, INF, -4 },
-	//	{ INF, 0, INF, 1, 7 },
-	//	{ INF, 4, 0, INF, INF },
-	//	{ 2, INF, 5, 0, INF },
-	//	{ INF, INF, INF, 6, 0 }
-	//};
-
-	//cout << "Adjacency Matrix" << endl;
-	//for (int i = 0; i < adjacencyMatrix.size(); i++)
-	//{
-	//	for (int j = 0; j < adjacencyMatrix.size(); j++)
-	//	{
-	//		cout << setw(8) << adjacencyMatrix[i][j] << "";
-	//	}
-
-	//	cout << endl;
-	//}
-
-	//Algorithm::Ptr parallelSolver(new ParallelAlgorithm(NUMBER_OF_PROCESSORS));
-	//auto parallelResult = parallelSolver->solve(adjacencyMatrix, 0, VERTICES);
-
-	//cout << format("Parallel alogirthm:\n{}\n", parallelResult.toString());
-
-	//result.assert(parallelResult);
 }
